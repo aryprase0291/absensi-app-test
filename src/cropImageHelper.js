94e7332ffe1a654a162c9c266b58c560b1bd69f1
prasetyo <aryprasetyosk@gmail.com>
2025-12-09 19:23:02 +0700

    initial commit

---
+++ src/cropImageHelper.js
@@ -0,0 +1,49 @@
+// cropImageHelper.js
+
+export const createImage = (url) =>
+  new Promise((resolve, reject) => {
+    const image = new Image();
+    image.addEventListener('load', () => resolve(image));
+    image.addEventListener('error', (error) => reject(error));
+    image.setAttribute('crossOrigin', 'anonymous'); 
+    image.src = url;
+  });
+
+export function getRadianAngle(degreeValue) {
+  return (degreeValue * Math.PI) / 180;
+}
+
+// PERHATIKAN: Kita pakai 'export async function', BUKAN 'export default'
+export async function getCroppedImg(imageSrc, pixelCrop, rotation = 0) {
+  const image = await createImage(imageSrc);
+  const canvas = document.createElement('canvas');
+  const ctx = canvas.getContext('2d');
+
+  const maxSize = Math.max(image.width, image.height);
+  const safeArea = 2 * ((maxSize / 2) * Math.sqrt(2));
+
+  canvas.width = safeArea;
+  canvas.height = safeArea;
+
+  ctx.translate(safeArea / 2, safeArea / 2);
+  ctx.rotate(getRadianAngle(rotation));
+  ctx.translate(-safeArea / 2, -safeArea / 2);
+
+  ctx.drawImage(
+    image,
+    safeArea / 2 - image.width * 0.5,
+    safeArea / 2 - image.height * 0.5
+  );
+  const data = ctx.getImageData(0, 0, safeArea, safeArea);
+
+  canvas.width = pixelCrop.width;
+  canvas.height = pixelCrop.height;
+
+  ctx.putImageData(
+    data,
+    Math.round(0 - pixelCrop.x),
+    Math.round(0 - pixelCrop.y)
+  );
+
+  return canvas.toDataURL('image/jpeg');
+}
