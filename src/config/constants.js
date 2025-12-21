43543d66ff559d59ece1f1b1c8d354c788e4d7e1
aryprasetyo <ariieelink@gmail.com>
2025-12-21 10:13:41 +0700

    update script url

--- src/config/constants.js
+++ src/config/constants.js
@@ -2,7 +2,7 @@ import {
   CheckCircle, LogOut, FileText, AlertTriangle, Clock, Briefcase, Calendar 
 } from 'lucide-react';
 
-export const SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbzxh0zM3kGlngh71OfHDVoshwlnSvP5_6fuWWX_VwVb0hgk5ZpeQbHyG8gqSiRLxFFHyw/exec';
+export const SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbz_JNls0575DT3yMQBMutVxCFzMgjrflZsmPIH-EXQ4XqG-75oNIINIUg62ZG29WRqw0w/exec';
 
 export const TIMEOUT_DURATION = 5 * 60 * 1000; 
 
