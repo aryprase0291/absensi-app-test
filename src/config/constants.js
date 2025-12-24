065b7d5dfe5027072e3d5b2cd80045ee99e814ac
aryprasetyo <ariieelink@gmail.com>
2025-12-24 14:42:57 +0700

    koreksi email konfirmasi

--- src/config/constants.js
+++ src/config/constants.js
@@ -2,7 +2,7 @@ import {
   CheckCircle, LogOut, FileText, AlertTriangle, Clock, Briefcase, Calendar 
 } from 'lucide-react';
 
-export const SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbwarkXU5X5oP1lqFgqGbkHHZ-hD251xcYlLDjvBs3KYLiEIotvYriyyrYBKAiHWiy7qNQ/exec';
+export const SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbwh7cY2zEnuDJn1lJyPYC9IotN6rJecgVW8N7aupmvGDw1J-s__ufB8qR9pYD7-D_SFkA/exec';
 
 export const TIMEOUT_DURATION = 5 * 60 * 1000; 
 
